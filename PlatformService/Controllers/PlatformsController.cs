e8f9b99cb5154c699e1fdd5e1de8472c4cdccfcd
DESKTOP-TKL80ML\KARA JEDINI <akarahasan3@etf.unsa.ba>
2021-11-01 18:33:11 +0100

    Project initialization + kubernetes setup

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Model;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting platforms...");
+            var platforms = _repository.GetAllPlatforms();
+            if (platforms == null) return NotFound("No platforms were found");
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platform = _repository.GetPlatformById(id);
+            if (platform == null) return NotFound("No platform was found");
+            return Ok(_mapper.Map<PlatformReadDto>(platform));
+        }
+        
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platform = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platform);
+            _repository.SaveChanges();
+
+            var platformCreated = _mapper.Map<PlatformReadDto>(platform);
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformCreated.Id}, platformCreated);
+        }
+    }
+}
