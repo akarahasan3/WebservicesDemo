4e3be0deb01ac8158b211901679278701818d062
DESKTOP-TKL80ML\KARA JEDINI <akarahasan3@etf.unsa.ba>
2021-11-30 08:29:49 +0100

    RabbitMQ initial setup

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Model;
@@ -17,12 +18,14 @@ namespace PlatformService.Controllers
         private readonly IPlatformRepo _repository;
         private readonly IMapper _mapper;
         private readonly ICommandDataClient _commandDataClient;
+        private readonly  IMessageBusClient _messageBusClient;
 
-        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient)
+        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient, IMessageBusClient messageBusClient)
         {
             _repository = repository;
             _mapper = mapper;
             _commandDataClient = commandDataClient;
+            _messageBusClient = messageBusClient;
         }
 
         [HttpGet]
@@ -51,6 +54,7 @@ namespace PlatformService.Controllers
 
             var platformCreated = _mapper.Map<PlatformReadDto>(platform);
 
+            //Send sync message
             try
             {
                 await _commandDataClient.SendPlatformToCommand(platformCreated);
@@ -58,6 +62,19 @@ namespace PlatformService.Controllers
             catch(Exception ex)
             {
                 Console.WriteLine($"--> Could not send synchronously: {ex.Message}");
+            }
+            
+            //Send async message
+            try
+            {
+                var platformPublishedDto = _mapper.Map<PlatformPublishedDto>(platformCreated);
+                platformPublishedDto.Event = "Platform_Published";
+                _messageBusClient.PublishNewPlatform(platformPublishedDto);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Could not send synchronously: {ex.Message}");
+                throw;
             }
             
             return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformCreated.Id}, platformCreated);
